951deac25f798c7acef6c0005e689f7b81818a9e
NipunLakshitha1999 <66331090+NipunLakshitha1999@users.noreply.github.com>
2023-04-27 12:10:52 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,19 @@
 
 public class Main {
 
-    public void test3(){}
+    public void test9(){
+    //test
+    }
 
-    public void test3(){}
+    public void test8(){
+    //test
+    }
     
-    public void test3(){}
+    public void test7(){
+    //test
+    }
     
     public static void main(String[] args) {
-       
+       //test
     }
 }
