a4722abfa8b0538d4c1dbe0291bc1e11aaff9ee7
Nipun Lakshitha <nipunlakshithasilva1999@gmail.com>
2023-04-27 12:24:40 +0530

    testttvhhghj

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,12 @@
 
 public class Main {
 
-<<<<<<< HEAD
-    public void tes3(){}
-
-    public void tes4(){}
-
-    public void tes5(){}
-
-=======
-    public void test9(){
-    //test
+    public void myTest(){
+        //hello
     }
->>>>>>> 951deac25f798c7acef6c0005e689f7b81818a9e
 
-    public void test8(){
-    //test
-    }
-    
-    public void test7(){
-    //test
-    }
-    
+
     public static void main(String[] args) {
-       //test
+       System.out.println("my");
     }
 }
